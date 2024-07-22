aaf124a4a7b67ea61ee6bef66c881837b0f06287
Ismael Polanco <1115279@est.intec.edu.do>
2024-07-22 16:30:01 -0400

    Cambio de img por dependencia de Image de next, se agrego 'flex flex-row' a contenedor de SIGN IN y a ese texto se cambio el padding izquierdo. La barra de procesos se cambio el borde a trasnparente y agregar hover

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Header() {
     return (
       <main>
@@ -9,20 +11,25 @@ export default function Header() {
             
             <div className="flex justify-center h-9">
               <a href="#" className="flex items-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/VOGUE_LOGO.svg/2560px-VOGUE_LOGO.svg.png" className="h-16" alt="Vogue logo" />
-              </a>
+              <Image src="/logo_preview.png" width={400} height={32} alt="Amy's Folio logo" className="h-16" />              </a>
             </div>
             
             <div className="flex items-center justify-end lg:order-2">
               <div className="border-r border-gray-300 pr-2">
-                <a href="#" className="text-gray-800 bg-primary-700 hover:bg-primary-800 font-medium text-sm px-4 lg:px-5 py-1 mr-2">SUSCRIBE</a>
+                <a href="#" className="text-gray-800 bg-primary-700 hover:bg-primary-800 font-medium text-sm py-1 mr-2">
+                  SUBSCRIBE
+                </a>
               </div>
-              <a href="#" className="text-gray-800 hover:bg-gray-100 font-medium rounded-3xl text-sm px-4 lg:px-5 py-1 flex items-center">
-                <span className="material-symbols-outlined mr-1">
-                  person
-                </span>
-                SIGN IN
-              </a>
+
+              <div className="flex flex-row hover:bg-gray-100 rounded-3xl">
+                <span className="material-symbols-outlined mr-1 pl-4">
+                    person
+                  </span>
+                <a href="#" className="text-gray-800 hover:underline font-medium text-sm pr-5 lpy-1 flex items-center">
+                  SIGN IN
+                </a>
+              </div>
+              
               <span className="material-symbols-outlined">
                 search
               </span>
@@ -30,17 +37,17 @@ export default function Header() {
   
           </nav>
           <nav className="bg-white border-gray-200 lg:px-0 shadow-md">
-            <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl space-x-0">
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">FASHION</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">BEAUTY</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">CULTURE</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">LIVING</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">WEDDINGS</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">RUNWAY</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">SHOPPING</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">VIDEO</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">VOGUE CLUB</a>
-              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-2 hover:border-black">VOGUE WORLD</a>
+            <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl space-x-1">
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">FASHION</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">BEAUTY</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">CULTURE</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">LIVING</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">WEDDINGS</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">RUNWAY</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">SHOPPING</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">VIDEO</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">VOGUE CLUB</a>
+              <a href="#" className="text-gray-800 font-medium text-sm px-2 py-2 border-transparent border-b-4 hover:border-black">VOGUE WORLD</a>
             </div>
           </nav>
         </header>
